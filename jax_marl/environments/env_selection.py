f8298a2147370179542a581ed191f2d6927ea874
LukavdBoogaard <l.v.d.boogaard@student.tue.nl>
2025-05-09 19:35:44 +0200

    changed env_selection to allow longer sequences

--- jax_marl/environments/env_selection.py
+++ jax_marl/environments/env_selection.py
@@ -6,10 +6,12 @@ from jax_marl.environments.overcooked_environment import overcooked_layouts
 # Define global layouts
 AVAIL_LAYOUTS = overcooked_layouts
 
-def generate_sequence(sequence_length=2, strategy='random', layout_names=None, seed=None):
+def generate_sequence(sequence_length=None, strategy='random', layout_names=None, seed=None):
     """
     Generate a sequence of layouts for the agents to play on. 
     """ 
+    if sequence_length is None:
+        sequence_length = len(layout_names) if layout_names is not None else len(AVAIL_LAYOUTS)
 
     if layout_names is None:
         layouts = []
@@ -18,18 +20,34 @@ def generate_sequence(sequence_length=2, strategy='random', layout_names=None, s
     else:
         layouts = layout_names
             
-    # Assert that the sequence length is smaller or equal to the layouts
-    assert sequence_length <= len(layouts), "The sequence length is longer than the available layouts"
+    # Set seed if provided
+    if seed is not None:
+        random.seed(seed)
+    
+    selected_layouts = []
     
     if strategy == 'random':
-        # set seed
-        if seed is not None:
-            random.seed(seed)
-        selected_layouts = random.sample(layouts, sequence_length)
-        # reset seed
-        random.seed(None)
+        if sequence_length <= len(layouts):
+            selected_layouts = random.sample(layouts, sequence_length)
+        else:
+            # Select layouts with replacement
+            available = layouts.copy()
+            last_selected = None
+            
+            for _ in range(sequence_length):
+                if len(available) == 1 and available[0] == last_selected:
+                    selected_layouts.append(available[0])
+                    last_selected = available[0]
+                else:
+                    current_available = [l for l in available if l != last_selected]
+                    choice = random.choice(current_available)
+                    selected_layouts.append(choice)
+                    last_selected = choice
     elif strategy == 'ordered':
-        selected_layouts = layouts[:sequence_length]
+        if sequence_length <= len(layouts):
+            selected_layouts = layouts[:sequence_length]
+        else:
+            raise ValueError("Ordered strategy requires sequence_length <= number of available layouts")
     else:
         raise NotImplementedError("Strategy not implemented")
     
